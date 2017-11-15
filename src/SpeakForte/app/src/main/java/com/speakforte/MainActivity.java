e571668b61a8f6ab15f50272adbd4a3cdd0dae42
Kuku Teslim <teslim.kuku@accessbankplc.com>
2017-11-15 10:33:43 +0100

    Happy Birthday Coding Challenge

---
+++ src/SpeakForte/app/src/main/java/com/speakforte/MainActivity.java
@@ -0,0 +1,19 @@
+package com.speakforte;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private final String LOG_TAG = "MainActivity.class";
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "Hello World");
+    }
+}
